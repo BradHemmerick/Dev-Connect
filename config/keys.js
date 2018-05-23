a856ee61eb8fc0ba9edeee3ff38aa7c270007f60
brad.hemmerick13@gmail.com <brad.hemmerick13@gmail.com>
2018-05-23 15:08:52 -0500

    set up connection to mongo, set up basic routes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'YOUR MONGODB CONNECTION'
+};
--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+//require routes
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
+
+
 
 const app = express();
 
+//db config
+const db = require('./config/keys').mongoURI
+//connect to db
+mongoose
+    .connect(db)
+        .then(() => console.log('Connected to MongoDB'))
+        .catch(err => console.log(`Failed to connected to DB to to err: ${err}`));
+
 app.get('/', (req, res) => res.send('Hello World!'));
+
+// Use routes
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts',  posts)
 
 const port = process.env.PORT || 3000
 
